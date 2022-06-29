bd939496adc7c60444e5245f55cede1a6921ab35
MD Sakhawat Hosen <79786846+aluminumkinley@users.noreply.github.com>
2022-06-29 14:47:59 +0600

    Update Utils.java

--- utils/src/main/java/com/example/utilsdg/Utils.java
+++ utils/src/main/java/com/example/utilsdg/Utils.java
@@ -1,5 +1,10 @@
 package com.example.utilsdg;
 
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+import java.lang.Math;
+
+
 public class Utils {
     private static String dummyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
@@ -34,7 +39,7 @@ public class Utils {
     public static String getRGB(int rgb){
         return String.format("#%06X", (0xFFFFFF & rgb));
     }
-    public static class Math {
+    public static class Maths {
         
         public static float epsilon = .0009f;
 
